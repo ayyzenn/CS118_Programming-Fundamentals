e5ea7d873f0f3dff3c76e03ff3d8a3ddae5e1f85
Saad Ahmad <saad.ahmad1024@gmail.com>
2021-09-03 10:46:22 +0500

    Add files via upload

---
+++ Codes/C++/hello_world.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h> // header file
+#include <iostream> // header file
+
+using namespace std; // namespace for cout
+
+int main()
+{
+    printf("Hello World!\n");
+    cout << "hello from cout" << endl;
+    cout << "Another line" << endl;
+    cout<< "Saad Ahmad" << endl;
+    cout<< "I am the Best" << endl;
+    return 0;
+}
